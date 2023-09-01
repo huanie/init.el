f4f1bffc94486a85744683d9439289625d35dd85
Huan Nguyen <goafanxx@gmail.com>
2023-09-01 13:38:02 +0200

    sdfjsdkfjsdf

---
+++ early-init.el
@@ -0,0 +1,47 @@
+;;; early-init.el --- Early init                     -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023  Huan Thieu Nguyen
+
+;; Author: Huan Thieu Nguyen <huantn@fedora>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(setq use-dialog-box t
+      use-file-dialog t
+      inhibit-splash-screen t
+      inhibit-startup-screen t
+      inhibit-startup-buffer-menu t)
+
+(tool-bar-mode -1)
+
+;; Temporarily increase the garbage collection threshold.  These
+;; changes help shave off about half a second of startup time.
+(defvar normal-gc-cons-threshold gc-cons-threshold)
+
+(setq gc-cons-threshold most-positive-fixnum)
+
+(add-hook 'emacs-startup-hook
+	  (lambda ()
+	    (setq gc-cons-threshold normal-gc-cons-threshold)))
+
+(setq package-enable-at-startup t)
+
+;;; early-init.el ends here
--- init.el
+++ init.el
@@ -4,32 +4,17 @@
 
 ;;; Code:
 (require 'package)
-(require 'org)
 (require 'use-package)
 
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
-;; and `package-pinned-packages`. Most users will not need or want to do this.
-;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
 (setq elisp-flymake-byte-compile-load-path user-emacs-directory)
 
 (setq use-package-always-ensure t)
 
-(use-package gcmh
-  :custom
-  (read-process-output-max (* 64 1024))
-  (gcmh-idle-delay 'auto)
-  (gcmh-auto-idle-delay-factor 10)
-  (gcmh-high-cons-threshold (* 16 1024 1024))
-  :functions gcmh-mode
-  :config
-  (gcmh-mode 1))
-
 (use-package gnu-elpa-keyring-update)
 
-(defvar tangled-file (concat user-emacs-directory "init-tangled.el"))
+(defvar tangled-file (locate-user-emacs-file "init-tangled.el"))
 
 (defun huan-tangle-config ()
   "Tangle the config file.
@@ -38,7 +23,7 @@ Basically inserting the code blocks into a file specified in the org document."
   (let ((org-confirm-babel-evaluate nil))
     (org-babel-tangle)))
 
-(setq custom-file (concat user-emacs-directory "custom.el"))
+(setq custom-file (locate-user-emacs-file "custom.el"))
 (when (file-exists-p custom-file)
   (load (expand-file-name custom-file)))
 
@@ -53,7 +38,7 @@ Basically inserting the code blocks into a file specified in the org document."
 (if (file-exists-p tangled-file)
     (load tangled-file)
   (progn
-    (org-babel-tangle-file (concat user-emacs-directory "config.org") tangled-file)
+    (org-babel-tangle-file (locate-user-emacs-file "config.org") tangled-file)
     (load tangled-file)))
 
 ;;; init.el ends here
